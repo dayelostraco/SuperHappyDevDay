56a205fe90472a192a9d711ebcf771260de7882b
Dayel Ostraco <dayel.ostraco@gmail.com>
2012-09-06 12:51:51 -0400

    fixed android codova native issues

--- SHDD-Android/src/com/sparcedge/shdd/android/CordovaActivity.java
+++ SHDD-Android/src/com/sparcedge/shdd/android/CordovaActivity.java
@@ -8,6 +8,6 @@ public class CordovaActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/login.html");
+        super.loadUrl("file:///android_asset/www/jQueryMobileTemplate.html");
     }
 }
